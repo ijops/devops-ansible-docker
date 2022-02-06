fee653df2d6509004d0c50b88c3c537caa047d8b
IJAZ MOHAMMED <talktoijaz@gmail.com>
2022-02-06 13:17:10 +0000

    docker.sh edited online with Bitbucket

--- docker.sh
+++ docker.sh
@@ -1,4 +1,4 @@
-containers=$(docker ps -a -q --filter="name=java-app1")
+containers=$(docker ps -a -q --filter="name=devops-docker-javaapp")
  if [ ! -z $containers ]; then
   docker stop $containers;
   docker rm $containers;
